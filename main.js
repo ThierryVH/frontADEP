fa7c3ed3349763c0b68bd5943c29147fbd35b5fb
Thierry Van-hecke <thierry.vhecke@gmail.com>
2017-08-01 19:53:26 +0200

    creation repo + nav desktop + header desktop

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function w3_open() {
+  document.getElementsByClassName("mySidebar")[0].style.display = "block";
+  document.getElementsByClassName("mySidebar")[1].style.display = "block";
+}
+function w3_close() {
+  document.getElementsByClassName("mySidebar")[0].style.display = "none";
+  document.getElementsByClassName("mySidebar")[1].style.display = "none";
+}
