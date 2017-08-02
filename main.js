30f75ac4fd8db697f2e58c813ba60174050b8f51
Thierry Van-hecke <thierry.vhecke@gmail.com>
2017-08-02 11:20:44 +0200

    test mobile nav

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ function w3_close() {
   document.getElementsByClassName("mySidebar")[0].style.display = "none";
   document.getElementsByClassName("mySidebar")[1].style.display = "none";
 }
+
+// var search = document.getElementById('recherche');
+//
+// var firstInterval = setInterval(deletePlaceholder, 20);
+// var secondInterval = setInterval(placeholder, 20);
+//
+// function deletePlaceholder(){
+//   if(window.innerWidth <= 1070){
+//     search.placeholder = "";
+//     clearInterval(firstInterval);
+//     secondInterval = setInterval(placeholder, 20);
+//   }
+// }
+//
+// function placeholder(){
+//   if(window.innerWidth > 1070){
+//     search.placeholder = "Recherche";
+//     clearInterval(secondInterval);
+//     firstInterval = setInterval(deletePlaceholder, 20);
+//   }
+// }
